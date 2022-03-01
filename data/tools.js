cf6b3644f87f94203adcbdeac0c4d0538b9dbfc6
unknown <felipelmrobles03@gmail.com>
2022-03-01 15:51:50 -0300

    portfolio icons addon

---
+++ data/tools.js
@@ -0,0 +1,29 @@
+const Tools = [
+    {
+        name: 'HTML5',
+        src: '../images/tools/html.png',
+        color: '#FF4E1D'
+    },
+    {
+        name: 'CSS3',
+        src: '../images/tools/css.png',
+        color: '#0074BE'
+    },
+    {
+        name: 'JavaScript',
+        src: '../images/tools/js.png',
+        color: '#F7DF1E'
+    },
+    {
+        name: 'React.js',
+        src: '../images/tools/react.png',
+        color: '#61DAFB'
+    },
+    {
+        name: 'Styled Components',
+        src: '../images/tools/styled.png',
+        color: '#242424'
+    }
+]
+
+export default Tools
--- script.js
+++ script.js
@@ -1,6 +1,36 @@
+import Tools from "./data/tools.js"
 
 const sidebarToggle = () => {
     const Sidebar = document.querySelector('.sidebar')
 
     Sidebar.classList.toggle('on')
 }
+
+const navbarScroll = () => {
+    const Navbar = document.querySelector('nav')
+
+    window.onscroll = isScroll
+
+    function isScroll() {
+        if (window.scrollY < 200) {
+            Navbar.classList.remove('scroll')
+        } else {
+            Navbar.classList.add('scroll')
+        }
+    }
+}
+navbarScroll()
+
+const ToolsSlider = () => {
+    const toolSlider = document.querySelector('.tool-slider')
+
+    Tools.map((item, index) => {
+        toolSlider.innerHTML += `
+            <button class="tool" style="background: ${item.color};">
+                <img src="${item.src}"/>
+                <div class="tool-gradient"></div>
+            </button>
+        `
+    })    
+}
+ToolsSlider()
