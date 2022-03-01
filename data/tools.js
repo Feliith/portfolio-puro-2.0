ece1ac25538964661b003ab612f6e0ff4d121bb1
unknown <felipelmrobles03@gmail.com>
2022-03-01 16:50:43 -0300

    scroll href addon

--- data/tools.js
+++ data/tools.js
@@ -1,26 +1,31 @@
 const Tools = [
     {
         name: 'HTML5',
+        id: 'html',
         src: '../images/tools/html.png',
         color: '#FF4E1D'
     },
     {
         name: 'CSS3',
+        id: 'css',
         src: '../images/tools/css.png',
         color: '#0074BE'
     },
     {
         name: 'JavaScript',
+        id: 'js',
         src: '../images/tools/js.png',
         color: '#F7DF1E'
     },
     {
         name: 'React.js',
+        id: 'react',
         src: '../images/tools/react.png',
         color: '#61DAFB'
     },
     {
         name: 'Styled Components',
+        id: 'styled',
         src: '../images/tools/styled.png',
         color: '#242424'
     }
--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 import Tools from "./data/tools.js"
 
+const sidebarClick = () => {
+    const navBurger = document.querySelector('.nav-burger')
+    const Sidebar = document.querySelector('.sidebar')
+
+    navBurger.addEventListener('click', () => {
+        sidebarToggle()
+    })
+
+    Sidebar.addEventListener('click', () => {
+        sidebarToggle()
+    })
+}
+sidebarClick()
+
 const sidebarToggle = () => {
     const Sidebar = document.querySelector('.sidebar')
 
@@ -26,11 +40,34 @@ const ToolsSlider = () => {
 
     Tools.map((item, index) => {
         toolSlider.innerHTML += `
-            <button class="tool" style="background: ${item.color};">
-                <img src="${item.src}"/>
-                <div class="tool-gradient"></div>
-            </button>
+            <div class="tool" id="${item.id}">
+                <button class="tool-btn" style="background: ${item.color};">
+                    <img src="${item.src}"/>
+                    <div class="tool-gradient"></div>
+                </button>
+                <h3 class="tool-name">${item.name}</h3>
+            </div>
         `
     })    
 }
 ToolsSlider()
+
+const ballClickEvent = () => {
+    const allBall = document.querySelectorAll('.ball')
+
+    for(let i = 0 ; i < allBall.length ; i++) {
+        allBall[i].addEventListener('click', () => {
+            ballClick(i)
+        })
+    }
+}
+ballClickEvent()
+
+const ballClick = (id) => {
+    const allBall = document.querySelectorAll('.ball')
+
+    const ballOn = document.querySelector('.ball.on')
+    ballOn.classList.remove('on')
+
+    allBall[id].classList.add('on')
+}
